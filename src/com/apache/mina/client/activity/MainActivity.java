3940e4f1a7d794e50de0fa23be4f888eaee58c96
hjkk1987 <hjkk19872>
2015-06-07 20:17:35 +0800

    Signed-off-by: hjkk1987 <hjkk19872>

--- src/com/apache/mina/client/activity/MainActivity.java
+++ src/com/apache/mina/client/activity/MainActivity.java
@@ -11,8 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		System.out.println("activity start" + android.os.Process.myPid() + '-'
-				+ android.os.Process.myTid());
+
 	}
 
 }
